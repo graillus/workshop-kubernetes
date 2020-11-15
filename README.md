ce9056392e1b243ed0943f3937662f5b0644deab
rgraillon <rgraillon@eleven-labs.com>
2020-11-15 18:24:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,66 @@
+Kubernetes Workshop
+===================
+
+### Prerequisites
+
+- [docker](https://docs.docker.com/engine/install/)
+- [kubectl](https://kubernetes.io/docs/tasks/tools/install-kubectl/)
+
+### Install local Kubernetes cluster
+
+We will install `kind` (Kubernetes IN Docker)
+```bash
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.9.0/kind-$(uname)-amd64
+chmod +x kind
+sudo mv kind /usr/local/bin
+```
+Create a local cluster
+```bash
+kind create cluster --name workshop --config cluster.yaml
+```
+
+### Running example application
+
+Build the docker image
+```bash
+docker build -t graillus/workshop-kubernetes:app-v1 docker-images/app
+```
+
+Load the image into the Kind nodes
+```bash
+kind load docker-image graillus/kubernetes-workshop:app-v1
+```
+
+Create a pod running the app
+```bash
+kubectl run my-app --image=graillus/workshop-kubernetes:app-v1 --port=3000
+```
+
+Watch created pod
+```bash
+kubectl get pods --watch
+```
+
+Expose pod endpoint to localhost
+```bash
+kubectl expose pod my-app --port=8080 --target-port=3000 --name=my-app
+```
+
+
+### Ingress Controller
+
+Install nginx ingress controller
+```bash
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
+```
+
+Watch created pod
+```bash
+kubectl get pods --watch --namespace ingress-nginx
+```
+
+### Cleanup
+
+```bash
+kind delete cluster workshop
+```
---
+++ docker-images/app/index.js
@@ -0,0 +1,81 @@
+const os = require('os')
+const express = require('express')
+const app = express()
+
+const VERSION = 'v1.0'
+
+app.use((req, res, next) => {
+  next()
+  console.log(`${req.method} ${req.path} ${res.statusCode}`)
+})
+
+app.get('/', (_, res) => {
+  const content = `
+    <h1>Hello, world!</h1>
+    <p>
+      App is running on host ${os.hostname()}
+    </p>
+    <a class="btn btn-danger" href="/terminate" role="button">Terminate server</a>
+  `
+  res.send(renderHtml(content))
+})
+
+app.get('/terminate', (_, res) => {
+  const content = `
+  <div class="alert alert-danger" role="alert">
+    Something wrong happended and process is going to exit !
+  </div>
+  <a class="btn btn-primary" href="/" role="button">Go back to homepage</a>
+  `
+  res.send(renderHtml(content))
+
+  process.exit(123)
+})
+
+app.listen(3000, '0.0.0.0', () => {
+  console.log('App listening on port 3000')
+})
+
+const renderHtml = content => {
+  return `
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+      integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>Workshop Kubernetes</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-dark bg-dark">
+      <span class="navbar-brand mb-0 h1">Workshop Kubernetes</span>
+    </nav>
+
+    <main role="main" class="container">
+      <div class="row mt-3">
+        <div class="col">
+        ${content}
+        </div>
+      </div>
+    </main>
+
+    <footer class="footer bg-light">
+      <div class="container">
+        <span class="text-muted">App version ${VERSION}</span>
+      </div>
+    </footer>
+    <style type="text/css">
+      .footer {
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+        /* Set the fixed height of the footer here */
+        height: 60px;
+        line-height: 60px; /* Vertically center the text there */
+      }
+    </style>
+  </body>
+</html>
+  `
+}
